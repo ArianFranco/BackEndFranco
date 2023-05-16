0cd858363a912b02e30533a4e959f7820d366aa9
Arian Franco <ariifre2@gmail.com>
2023-05-15 23:50:22 -0300

    server

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,79 +1,39 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const productos = require('../DB.json');
-const { checkAdmin } = require("../utils");
+const Product = require('../dao/models/product');
 
-router.get("/productos/listar", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const prods = await productos.listar();
-    if (prods.length === 0) {
-      res.status(404).json({ error: "no hay productos cargados" });
+    const limit = parseInt(req.query.limit) || 10;
+    const page = parseInt(req.query.page) || 1;
+    const sort = req.query.sort || '';
+    const query = req.query.query || '';
+
+    const skip = (page - 1) * limit;
+    let products;
+
+    if (query) {
+      products = await Product.find({ title: { $regex: query, $options: 'i' } })
+        .sort(sort === 'desc' ? { price: -1 } : { price: 1 })
+        .skip(skip)
+        .limit(limit);
     } else {
-      res.status(200).json(prods);
+      products = await Product.find()
+        .sort(sort === 'desc' ? { price: -1 } : { price: 1 })
+        .skip(skip)
+        .limit(limit);
     }
-  } catch (error) {
-    res.status(500).send({ error: error.message });
-  }
-});
 
-router.get("/productos/listar/:id", async (req, res) => {
-  try {
-    const producto = await productos.listarPorId(req.params.id);
-    if (!producto) {
-      res.status(404).json({ error: "producto no encontrado" });
-    } else {
-      res.status(200).json(producto);
-    }
+    return res.send({
+      status: 'success',
+      products,
+    });
   } catch (error) {
-    res.status(500).send({ error: error.message });
-  }
-});
-
-router.post("/productos/guardar", checkAdmin, async (req, res) => {
-  try {
-    const { title, price, thumbnail } = req.body;
-    if (!title || !price || !thumbnail) {
-      return res.status(400).json({ error: "faltan datos" });
-    }
-    const prod = await productos.guardar(title, price, thumbnail);
-    res.status(201).json(prod);
-  } catch (error) {
-    res.status(500).send({ error: error.message });
-  }
-});
-
-router.put("/productos/actualizar/:id", checkAdmin, async (req, res) => {
-  try {
-    const { title, price, thumbnail } = req.body;
-    if (!title || !price || !thumbnail) {
-      return res.status(400).json({ error: "faltan datos" });
-    }
-    const prod = await productos.actualizar(
-      req.params.id,
-      title,
-      price,
-      thumbnail
-    );
-    if (!prod) {
-      res.status(404).json({ error: "producto no encontrado" });
-    } else {
-      res.status(200).json(prod);
-    }
-  } catch (error) {
-    res.status(500).send({ error: error.message });
-  }
-});
-
-router.delete("/productos/borrar/:id", checkAdmin, async (req, res) => {
-  try {
-    const prod = await productos.borrar(req.params.id);
-    if (!prod) {
-      res.status(404).json({ error: "producto no encontrado" });
-    } else {
-      res.status(200).json(prod);
-    }
-  } catch (error) {
-    res.status(500).send({ error: error.message });
+    console.error(error);
+    res.status(500).send({
+      status: 'error',
+      message: 'Error al obtener los productos',
+    });
   }
 });
 
